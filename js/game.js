41ec1bab886f833c113af1b1223d9d0043af0ce8
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-04-10 11:35:26 +0530

    function renderboard is created

--- js/game.js
+++ js/game.js
@@ -11,3 +11,29 @@ const playerImage = {
     1: "circle.png",
     2: "cross.png",
 };
+
+function renderBoard() {
+    const boardElement = document.getElementById("board");
+    boardElement.innerHTML = "";
+
+    const playerElement = document.getElementById("player-element");
+    if (!gameOver) {
+        playerElement.innerHTML = `Player ${turn}'s Turn`;
+    } else {
+        playerElement.innerHTML = "Game Over!";
+    }
+
+    board.forEach((cellValue, index) => {
+        const cellPlayerImage = playerImage[cellValue];
+        const cellImage = cellPlayerImage ? `<img src="${cellPlayerImage}" />` : "";
+
+        const cellClassName = cellValue === -1 ? "" : cellClass[cellValue] || "";
+        boardElement.innerHTML += `
+            <div class="cell ${cellClassName}" onclick="handleTurn(${index})">
+                ${cellImage}
+            </div>
+        `;
+    });
+
+    checkWinner();
+}
