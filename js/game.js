ec80d5d34687d2f2b1514326eaccc90e9660fb35
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-04-10 11:36:37 +0530

    function checkwinner is created

--- js/game.js
+++ js/game.js
@@ -51,3 +51,25 @@ function handleTurn(cellIndex) {
         alert("Invalid Move");
     }
 }
+
+function checkWinner() {
+    const PLAYER_1 = 1;
+    const PLAYER_2 = 2;
+
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
+    ];
+
+    for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a] !== -1 && board[a] === board[b] && board[a] === board[c]) {
+            endGame(`Player ${board[a]} Wins!`);
+            return;
+        }
+    }
+    if (!board.includes(-1)) {
+        endGame("It's a Draw!");
+    }
+}
