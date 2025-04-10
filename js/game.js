456ada3109c86b9b6782955c27c56ec539dc9172
Tushar Chindhe <tusharchindhe2002@gmail.com>
2025-04-10 11:34:00 +0530

    cellclass variable declared

--- js/game.js
+++ js/game.js
@@ -1,3 +1,8 @@
 const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-        let turn = 1;
-        let gameOver = false;
+let turn = 1;
+let gameOver = false;        
+
+const cellClass = {
+1: "cell-p1",
+2: "cell-p2",
+};
